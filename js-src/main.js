5d844af35ba5c994650bbd0a87831e27a9feddbb
Ben Mather <bwhmather@bwhmather.com>
2014-02-12 23:07:37 +0000

    start of high level control using jake gordons state machine library

--- js-src/main.js
+++ js-src/main.js
@@ -1,8 +1,27 @@
 "use strict";
 
+var StateMachine = require('./lib/state-machine.js');
+
 var Game = require('./game.js').Game;
 var data = require('./data.js');
 
 var canvas = document.getElementsByTagName('canvas')[0];
 var game = new Game(canvas, data);
-game.run();
+
+var fsm = StateMachine.create({
+  initial: 'loading',
+  events: [
+    { name: 'loaded', from: 'loading', to: 'mainMenu' },
+    { name: 'start', from: 'mainMenu', to: 'game' },
+    { name: 'pause', from: 'game', to: 'paused' },
+    { name: 'resume', from: 'paused', to: 'game' },
+    { name: 'crashed', from: 'game', to: 'mainMenu' }
+  ],
+  callbacks: {
+    onstart: function(event, from, to) { game.run(); }
+  }
+});
+
+document.getElementById('play-btn').onclick = function() {fsm.start()};
+
+fsm.loaded();
